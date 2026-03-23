6262cc61e5df9a49bdaede4ba61820b58823c642
rahultiwari8685 <rtiwari69572@gmail.com>
2026-03-23 13:51:21 +0530

    blog updated

--- backend/src/models/Blog.js
+++ backend/src/models/Blog.js
@@ -1,29 +1,23 @@
 import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema(
-    {
-        title: { type: String, required: true },
-        slug: { type: String, required: true, unique: true },
-        subtitle: { type: String, default: "" },
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    subtitle: { type: String, default: "" },
 
-        categories: [
-            { type: mongoose.Schema.Types.ObjectId, ref: "Category" }
-        ],
+    categories: [{ type: mongoose.Schema.Types.ObjectId, ref: "Category" }],
 
+    type: { type: String, enum: ["1", "2"], default: "1" },
 
+    videoType: { type: String, enum: ["1", "2"], default: "1" },
+    youtubeUrl: { type: String, default: "" },
 
+    content: { type: Object, default: {} },
 
-        type: { type: String, enum: ["1", "2"], default: "1" },
-
-        videoType: { type: String, enum: ["1", "2"], default: "1" },
-        youtubeUrl: { type: String, default: "" },
-
-        content: { type: Object, default: {} },
-
-        thumbnail: { type: String, default: "" },
-
-    },
-    { timestamps: true }
+    thumbnail: { type: String, default: "" },
+  },
+  { timestamps: true },
 );
 
 export default mongoose.model("Blog", blogSchema);
